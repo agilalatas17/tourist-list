71740b13b486b1aaa3e5dfd09e9cda7362502bef
gilangmukharom <gilangmukharom53@gmail.com>
2024-01-20 17:20:06 +0700

    adding scss in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import react from '@vitejs/plugin-react';
 import ViteSassPlugin from 'vite-plugin-sass';
 
 export default {
-  plugins: [react(), ViteSassPlugin()],
+  plugins: [
+    react(),
+    ViteSassPlugin({
+      include: /\.scss$/,
+      exclude: /node_modules/,
+    }),
+  ],
 };
